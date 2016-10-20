3bc9b7338cb46ec1a45dc7af598914f96306f298
Jakob Fix <jakob@jfix.com>
2016-10-20 17:23:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+redmine-notify-client
+=====================
+
+Tiny Javascript client that connects to a message broker (tested with ActiveMQ) via the obscure but up-and-coming mqtt protocol.  There it listens on a topic for any incoming messages.  When there are messages, it will inspect them and based on their content take the appropriate action.
+
+At the moment, it will play sounds as follows:
+* applaud if a Redmine issue has been closed
+* boo if a Redmine issue was reopened
+* sound an alarm if a new issue was created that has a higher-than-normal priority
+
+It doesn't do much more. Although, I have my eyes on a Delcom visual indicator: https://www.delcomproducts.com/products_USBLMP.asp
+
+Also, it will probably be used with the `windows-service` npm module so that it runs as a Windows service.
+
+Audio files found here:
+* Boo sound effect: https://www.youtube.com/watch?v=PS_cV18z67Y
+* Applause: http://soundbible.com/989-10-Second-Applause.html
+* Siren: http://soundbible.com/1355-Warning-Siren.html
---
+++ mqtt.js
@@ -0,0 +1,74 @@
+var mqtt = require("mqtt");
+var player = require('play-sound')(opts = {})
+
+// adapt this to your needs
+var cfg = {
+  "broker_url": 'mqtt://localhost:1883',
+  "topic": "redmine",
+  "status": {
+    "closed": 5,
+    "reopened": 8
+  },
+  "priority": {
+    "aboveNormal": 4
+  }
+};
+
+
+// connect to the broker where the mqtt protocol endpoint is exposed
+var client = mqtt.connect(cfg.broker_url);
+
+client.on('connect', function(){
+  console.log("connected!");
+  client.subscribe(cfg.topic, function(err, granted){
+    if (!err) {
+      console.log("subscribed to " + cfg.topic);
+
+      client.on("message", function(topic, message){
+        var info = JSON.parse(message);
+
+        switch(info.type){
+          case "change":
+            if (info.new_status == cfg.status.closed) { // issue closed!!!
+              player.play('./sounds/applause.mp3');
+
+            } else if (info.new_status == cfg.status.reopenend) { // issue reopenend!!!!
+              player.play('./sounds/boo.mp3');
+            }
+
+            break;
+          case "creation":
+            if (info.priority > cfg.priority.aboveNormal) { // new urgent or high issue
+              player.play('./sounds/siren.mp3');
+            }
+            break;
+          default:
+            console.log("not creation nor change, mmmmhhhh ....");
+            // shouldn't get here
+        }
+      });
+    }
+  })
+});
+
+// I guess these may differ from Redmine to Redmine, these are ours
+
+/*
+  priorities:
+  3 - low
+  4 - normal
+  5 - high
+  6 - urgent
+
+*/
+
+/*
+  statuses:
+  1 - new
+  2 - assigned
+  3 - resolved
+  4 - feedback
+  5 - closed
+  6 - rejected
+  8 - reopenend
+*/
